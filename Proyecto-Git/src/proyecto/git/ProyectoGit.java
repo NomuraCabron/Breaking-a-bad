7be1dd9dcf7935525678d4ad9df46e88a946b12d
alexl <alexl@LAPTOP-K5F5FDFE>
2023-12-04 16:45:34 +0100

    Hemos puesto un scaner y dos variables

---
+++ Proyecto-Git/src/proyecto/git/ProyectoGit.java
@@ -0,0 +1,14 @@
+package proyecto.git;
+
+import java.util.Scanner;
+
+public class ProyectoGit {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int prova = 0;
+        String nomura = "Haz algo en condiciones";
+    }
+    
+}
